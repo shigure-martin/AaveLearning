9d4d2f730fc3add9cc06f270ffff5c0612248974
Martin <h_martin@163.com>
2023-03-13 17:57:07 +0800

    update the utils/constants

--- utils/constants.js
+++ utils/constants.js
@@ -3,13 +3,13 @@
  * @Author: Martin
  * @Date: 2023-02-27 15:03:40
  * @LastEditors: Martin
- * @LastEditTime: 2023-03-07 10:02:55
+ * @LastEditTime: 2023-03-09 17:28:45
  */
 const { BigNumber } = require("ethers");
 
 const decimal = BigNumber.from(10).pow(27);
 
-const RAY = 1 * 10 ** 27
+const RAY = BigNumber.from(1).mul(decimal);
 const ETHEREUM_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
 const APPROVAL_AMOUNT_LENDING_POOL_CORE = BigNumber.from(1).mul(decimal);
 const RATEMODE = {
